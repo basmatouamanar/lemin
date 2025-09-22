040fc2f9fd807670a315d8fa8fef73a4bfb7186e
btoumana <basmatoumanar55@gmail.com>
2025-09-22 10:14:24 +0100

    j

--- main.go
+++ main.go
@@ -24,27 +24,28 @@ func main() {
 	graphe := helpers.Graphe(rooms)
 	// fmt.Println(graphe, "heeee")
 	var save [][]string
-	
+
 	for {
 		path := helpers.Bfs(graphe, StartRoom, EndRoom)
 		if path == nil || len(path) == 0 {
 			break
 		}
-		fmt.Println(path)
+
 		save = append(save, path)
-		
-		for i := 0; i < len(path); i++ {
-			for j := 0; j < len(graphe[path[i]]); j++ {
-				m := graphe[path[i]][j]
-				if i+1 < len(path) && m == path[i+1] {
-					graphe[path[i]] = append(graphe[path[i]][:j], graphe[path[i]][j+1:]...)
-					j--
-					// fmt.Println(graphe[path[i]], "heeeeeeeeeeeeeeeeeee")
+
+		for i := 0; i < len(path)-1; i++ {
+			current := path[i]
+			next := path[i+1]
+
+			newNeighbors := []string{}
+			for _, neighbor := range graphe[current] {
+				if neighbor != next {
+					newNeighbors = append(newNeighbors, neighbor)
 				}
 			}
+			graphe[current] = newNeighbors
 		}
-		fmt.Println(save)
-		path = nil
 	}
+	fmt.Println(save)
 
 }
