42adbd65ff098812adc110b63e916d0bdd01e235
yassinoli <yassin890890.890@gmail.com>
2025-10-05 12:12:50 +0100

    new algorithm

--- main.go
+++ main.go
@@ -2,57 +2,63 @@ package main
 
 import (
 	"fmt"
+	"lem-in/Algorithms"
+	"lem-in/GlobVar"
+	"lem-in/Helpers"
+	"lem-in/Utils"
 	"os"
-	"strings"
+	"sort"
 )
 
-var Paths [][]string
 
-type Room struct {
-	Name string
-	X    int
-	Y    int
-}
-
-type Tunnel struct {
-	From string
-	To   string
-}
-
-type AntFarm struct {
-	Rooms   []Room
-	Tunnels []Tunnel
-	Start   Room
-	End     Room
-	Ants    int
-}
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("Usage: go run . <filename>")
-		return
-	}
+	args := os.Args[1:]
+    if len(args) != 1 {
+		fmt.Println("USAGE: go run . \"data.txt\"")
+        return // Exit if no input file is provided
+    }
 
-	antFarm, file, err := readInput(os.Args[1])
+	// Read the input file
+    dataBytes, err := os.ReadFile(args[0])
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("ERROR: invalid data format;", err)
 		return
 	}
 
-	findPaths(antFarm.Tunnels, antFarm.Start.Name, antFarm.End.Name, []string{})
-	if len(Paths) == 0 {
-		fmt.Println("ERROR: invalid path")
+	// Parse the input data and initialize global variables
+	err = Utils.ParsingData(string(dataBytes))
+
+
+	GlobVar.OriginalRooms = Helpers.CopyRoomsMap(GlobVar.Rooms)
+	if err != nil {
+		fmt.Println("ERROR: invalid data format;", err)
 		return
 	}
 
-	fmt.Println(strings.Join(file, "\n") + "\n")
+	Algorithms.FindValidPaths()
+	GlobVar.AllValidPaths = append(GlobVar.AllValidPaths, GlobVar.ValidPaths)
 
-	bestPaths := getBestPaths(Paths, antFarm.Ants)
-	if len(bestPaths) == 0 {
-		fmt.Println("ERROR: no usable paths")
-		return
+	// Sort valid paths by length (shortest first)
+	sort.Slice(GlobVar.ValidPaths, func(i, j int) bool {
+		return len(GlobVar.ValidPaths[i]) < len(GlobVar.ValidPaths[j])
+	})
+
+	// Assign ants to the shortest path and calculate the number of turns
+	shortestPathIndex := 0
+	lessTurns, antsOrdred := Algorithms.OrderAnts(0)
+
+	// Check other paths to find the one with the least number of turns
+	for i := 1; i < len(GlobVar.AllValidPaths); i++ {
+		turns, ants := Algorithms.OrderAnts(i)
+		if turns < lessTurns {
+			antsOrdred = ants
+			lessTurns = turns
+			shortestPathIndex = i
+		}
 	}
 
-	finalPaths, pathCounts := distributeAnts(antFarm.Ants, bestPaths)
-	printAnts(finalPaths, pathCounts)
+	// Print the results, including the input data and ant movements
+	Utils.HandleExport(antsOrdred,lessTurns,shortestPathIndex, string(dataBytes))
+	fmt.Println()
 }
